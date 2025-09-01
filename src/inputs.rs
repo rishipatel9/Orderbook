dd8d77165fbfc45b3564b62d0bf0d84b0317b921
Rishi <rishipatel0826@gmail.com>
2025-09-01 21:39:41 +0530

    Add global order management structures and enhance OrderBook with new fields

--- src/inputs.rs
+++ src/inputs.rs
@@ -1,6 +1,4 @@
 use std::collections::{BTreeMap};
-
-use actix_web::cookie::time::Time;
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize,Serialize,Debug)]
@@ -9,12 +7,17 @@ pub struct CreateOrderInput{
     pub price:f64,
     pub quantity:u32,
     pub user_id:u32,
-    pub side:Side
+    pub side:Side,
+    pub order_type:OrderType
 }
 
-#[derive(Deserialize,Serialize,Debug,Clone, Copy)]
+#[derive(Deserialize,Serialize,Debug,Clone, Copy,PartialEq)]
 pub enum Side{
     Buy,Sell
+}
+#[derive(Serialize,Deserialize,Debug,Clone,Copy,PartialEq)]
+pub enum OrderType{
+    Limit,Market
 }
 #[derive(Deserialize,Serialize,Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Symbol {
@@ -23,16 +26,17 @@ pub enum Symbol {
     SOLUSD
 }
 type OrderId = u64;
-type Price = u64;
+pub type Price = u64;
 type Quantity = u64;
 
-#[derive(Debug, Clone,Copy)]
+#[derive(Debug, Clone,Serialize,Deserialize)]
 pub struct Order {
     pub id: OrderId,
     pub price: Price,
     pub qty: Quantity,
     pub is_buy: bool,
-    pub time:Time
+    pub order_type:OrderType,
+    pub time:String,
 }
 
 #[derive(Debug)]
@@ -40,7 +44,30 @@ pub struct OrderBook {
     pub bids: BTreeMap<Price, Vec<Order>>, 
     pub asks: BTreeMap<Price, Vec<Order>>, 
     pub symbol:Symbol,
+    pub current_price:Option<Price>,
     pub last_trade_price: Option<Price>,
     pub current_best_bid: Option<Price>,
     pub current_best_ask: Option<Price>,
+}
+
+#[derive(Debug)]
+pub struct OrderBookDepth {
+    pub bids: Vec<(u64, u64)>, 
+    pub asks: Vec<(u64, u64)>, 
+}
+
+#[derive(Debug)]
+pub struct ProcessOrderResult {
+    pub order_id: u64,
+    pub trades: Vec<Order>,
+    pub remaining_quantity: u64,
+    pub orderbook_state: OrderBookState,
+}
+#[derive(Debug)]
+pub struct OrderBookState {
+    pub symbol: Symbol,
+    pub current_price: Option<u64>,
+    pub best_bid: Option<u64>,
+    pub best_ask: Option<u64>,
+    pub last_trade_price: Option<u64>,
 }
