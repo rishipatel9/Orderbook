00de9085188c6a03f96c5b18de2a175d003c2e0a
Rishi <rishipatel0826@gmail.com>
2025-09-01 21:39:04 +0530

    Refactor order processing to enhance response structure and error handling

--- src/bin/worker.rs
+++ src/bin/worker.rs
@@ -1,14 +1,18 @@
-use rand::{rngs::ThreadRng, Rng};
+use orderbook::{config::APP_CONFIG, engine::service::process_order};
+use rand::{ rngs::ThreadRng};
 use redis::{AsyncCommands, Client};
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use tokio::time::{sleep, Duration};
-use serde::{Serialize, Deserialize};
-use orderbook::{config::APP_CONFIG, engine::engine::process_order};
+use tokio::time::{Duration, sleep};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct OrderResponse {
-    order: String,   
-    result_id: u32,  
+    result_id: u64,
+    trades: Vec<serde_json::Value>,
+    remaining_quantity: u64,
+    current_price: Option<f64>,
+    best_bid: Option<f64>,
+    best_ask: Option<f64>,
 }
 
 #[tokio::main]
@@ -16,32 +20,53 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let redis_url = &APP_CONFIG.redis_url;
     let redis_client = Client::open(redis_url.clone())?;
     let mut conn = redis_client.get_async_connection().await?;
-    let mut rng: ThreadRng = rand::thread_rng();
-    
+    let mut _rng: ThreadRng = rand::thread_rng();
+
     loop {
         let result: redis::RedisResult<Vec<String>> = conn.blpop("order", 0).await;
         match result {
             Ok(items) => {
-                 if items.len() >= 2 {
+                if items.len() >= 2 {
                     let order_data = &items[1];
-                    
-                    let order_json: Value = serde_json::from_str(order_data)?;
-                    process_order(&order_json);
-                    let request_id = order_json["request_id"].as_str()
-                        .unwrap_or("unknown");
-                    
-                    let n: u32 = rng.gen_range(1..=100);
-                    let response = OrderResponse {
-                        order: order_data.clone(),
-                        result_id: n,
-                    };
 
-                    let json = serde_json::to_string(&response)?;
-                    
-                    let response_channel = format!("order_response:{}", request_id);
-                    let _: () = conn.publish(&response_channel, json).await?;
+                    let order_json: Value = serde_json::from_str(order_data)?;
+                    let request_id = order_json["request_id"].as_str().unwrap_or("unknown");
+
+                    match process_order(&order_json) {
+                        Ok(result) => {
+                            // let n: u32 = rng.gen_range(1..=100);
+                            let response = OrderResponse {
+                                result_id: result.order_id,
+                                trades: result.trades.iter()
+                                    .map(|t| serde_json::to_value(t).unwrap())
+                                    .collect(),
+                                remaining_quantity: result.remaining_quantity,
+                                current_price: result.orderbook_state.current_price
+                                    .map(|p| p as f64 / 100.0),
+                                best_bid: result.orderbook_state.best_bid
+                                    .map(|p| p as f64 / 100.0),
+                                best_ask: result.orderbook_state.best_ask
+                                    .map(|p| p as f64 / 100.0),
+                            };
+                            println!("{:?}",response);
+
+                            let json = serde_json::to_string(&response)?;
+
+                            let response_channel = format!("order_response:{}", request_id);
+                            let _: () = conn.publish(&response_channel, json).await?;
+                        }
+                        Err(e)=>{
+                            eprintln!("Error processing order :{}",e);
+                            let error_response = serde_json::json!({
+                                "error": e.to_string(),
+                                "result_id": 0
+                            });
+                            let response_channel = format!("order_response:{}", request_id);
+                            let _: () = conn.publish(&response_channel, error_response.to_string()).await?;
+                        }
+                    }
                 }
-            } 
+            }
             Err(e) => {
                 eprintln!("Error reading from Redis queue: {}", e);
                 sleep(Duration::from_secs(5)).await;
@@ -49,4 +74,3 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 }
-
